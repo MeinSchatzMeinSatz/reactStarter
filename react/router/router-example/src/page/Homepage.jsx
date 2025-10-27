d1fdb4c30f2da375fa19b0e54cda60007d54907b
Liebe_Chaeso <d131215@naver.com>
2025-10-27 11:47:13 +0900

    useSearchParams 사용법

--- react/router/router-example/src/page/Homepage.jsx
+++ react/router/router-example/src/page/Homepage.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Homepage = () => {
+  const navigate = useNavigate();
+
+  function goProductPage() {
+    navigate("/products?q=skirts");
+  }
+
   return (
     <div>
       <h1>HomePage</h1>
       <Link to="/about">go to About page</Link>
+      <button onClick={() => goProductPage()}>go to product page</button>
     </div>
   );
 };
