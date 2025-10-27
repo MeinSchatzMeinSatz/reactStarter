f364302c3f1631d06fc2d7be85c37c3223d407af
Liebe_Chaeso <d131215@naver.com>
2025-10-27 10:05:53 +0900

    라우터 앱 설치

--- react/router/router-example/src/App.jsx
+++ react/router/router-example/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Homepage from "./page/Homepage";
+import About from "./page/About";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Homepage />
+      <About />
+    </>
+  );
 }
 
 export default App;
