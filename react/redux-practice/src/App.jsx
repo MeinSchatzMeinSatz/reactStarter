3f0884197b732c86835a4d4663da995239c4dbfe
Liebe_Chaeso <d131215@naver.com>
2025-10-27 16:00:41 +0900

    기본적인 카운팅 앱 설정

--- react/redux-practice/src/App.jsx
+++ react/redux-practice/src/App.jsx
@@ -1,7 +1,17 @@
-import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>Counting App</h1>
+        <p>{count}</p>
+        <button onClick={() => setCount((prev) => prev + 1)}>+1</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
