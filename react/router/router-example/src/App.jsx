fc51c5e1ec6ae34e575b647c037269a796055fc6
Liebe_Chaeso <d131215@naver.com>
2025-10-27 13:44:23 +0900

    리다이렉트 시키기: Navigate 컴포넌트를 이용해서

--- react/router/router-example/src/App.jsx
+++ react/router/router-example/src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Homepage from "./page/Homepage";
 import About from "./page/About";
 import ProductPage from "./page/ProductPage";
 import ProductDetail from "./page/ProductDetail";
+import LoginPage from "./page/LoginPage";
+import UserPage from "./page/UserPage";
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(true);
+  const PrivateRoute = () => {
+    return authenticated ? <UserPage /> : <Navigate to="/login" />;
+  };
+
   return (
     <>
       <Routes>
@@ -13,6 +21,8 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<ProductPage />} />
         <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/user" element={<PrivateRoute />} />
       </Routes>
     </>
   );
